585659ddfac3aedbae77660cbe85a0b0ababb50d
tmuraoka320 <tmuraoka@wustl.edu>
2016-04-06 13:27:14 -0500

    MC

--- Muraoka_ProblemSet07.R
+++ Muraoka_ProblemSet07.R
@@ -14,7 +14,7 @@
 # dim = the number of dimension (sigle number)
 # lower = a vector of lower value in each dimension, whose length should match dim
 # upper = a vector of upper value in each dimension, whose length should match dim
-sg.int<-function(f, dim, lower, upper){
+sg.int <- function(f, dim, lower, upper){
   require("SparseGrid")
   lower <- floor(lower)
   upper <- ceiling(upper)
@@ -27,11 +27,11 @@ sg.int<-function(f, dim, lower, upper){
     stop("dim should be numeric!")
   }
   # if lower > upper, return error
-  if(any(lower > upper)){ 
+  if(any(lower > upper)){
     stop("lower must be smaller than upper!")
   }
   # if dim is different from the number of of elements in lower or upper, return error
-  if(any(dim != c(length(lower), length(upper)))){ 
+  if(any(dim != c(length(lower), length(upper)))){
     stop("dimension mismatches!")
   }
   # create a list that contains the sequence of elements in each dimension
@@ -43,7 +43,7 @@ sg.int<-function(f, dim, lower, upper){
   nodes <- gridss[1,] + sp.grid$nodes
   weights <- sp.grid$weights
   for (i in 2:nrow(gridss)){
-    nodes <- rbind(nodes, gridss[i,] + sp.grid$nodes)  
+    nodes <- rbind(nodes, gridss[i,] + sp.grid$nodes)
     weights <- c(weights, sp.grid$weights)
   }
   # evaluate function f in nodes
@@ -67,13 +67,13 @@ test_that("Correct Inputs", {
   example1 <- function(x){ # one-dimensional function
     return(x[1]^3)
   }
-  expect_error(sg.int("random", dim=1, lower=c(1), upper=c(20)), 
+  expect_error(sg.int("random", dim=1, lower=c(1), upper=c(20)),
                "f should be a function!")
-  expect_error(sg.int(example1, dim="a", lower=c(1), upper=c(20)), 
+  expect_error(sg.int(example1, dim="a", lower=c(1), upper=c(20)),
                "dim should be numeric!")
-  expect_error(sg.int(example1, dim=1, lower=c(20), upper=c(1)), 
+  expect_error(sg.int(example1, dim=1, lower=c(20), upper=c(1)),
                "lower must be smaller than upper!")
-  expect_error(sg.int(example1, dim=1, lower=c(1,0), upper=c(20,10)), 
+  expect_error(sg.int(example1, dim=1, lower=c(1,0), upper=c(20,10)),
                "dimension mismatches!")
 })
 # pass
@@ -101,7 +101,7 @@ test_that("Correct Outcome", {
 
 # the parallel version of the function sg.int
 # core = number of cores
-sg.int.parallel<-function(f, dim, lower, upper, core){
+sg.int.parallel <- function(f, dim, lower, upper, core){
   require("SparseGrid")
   require("parallel")
   cluster <- makeCluster(core)
@@ -116,11 +116,11 @@ sg.int.parallel<-function(f, dim, lower, upper, core){
     stop("dim should be numeric!")
   }
   # if lower > upper, return error
-  if(any(lower > upper)){ 
+  if(any(lower > upper)){
     stop("lower must be smaller than upper!")
   }
   # if dim is different from the number of of elements in lower or upper, return error
-  if(any(dim != c(length(lower), length(upper)))){ 
+  if(any(dim != c(length(lower), length(upper)))){
     stop("dimension mismatches!")
   }
   clusterExport(cluster, c("lower", "upper"))
@@ -133,7 +133,7 @@ sg.int.parallel<-function(f, dim, lower, upper, core){
   nodes <- gridss[1,] + sp.grid$nodes
   weights <- sp.grid$weights
   for (i in 2:nrow(gridss)){
-    nodes <- rbind(nodes, gridss[i,] + sp.grid$nodes)  
+    nodes <- rbind(nodes, gridss[i,] + sp.grid$nodes)
     weights <- c(weights, sp.grid$weights)
   }
   # evaluate function f in nodes
@@ -157,7 +157,7 @@ microbenchmark(
 )
 # Unit: milliseconds
 #    expr       min       lq      mean    median        uq       max neval cld
-#  1 core  188.6563  194.223  280.1186  206.8532  305.4991  881.1623    20  a 
+#  1 core  188.6563  194.223  280.1186  206.8532  305.4991  881.1623    20  a
 #  2 core 1705.1957 1778.791 2278.3751 1921.6250 2404.8963 3797.5963    20   b
 # parallel is significantly slower!
 
@@ -168,7 +168,7 @@ example3 <- function(x){
 
 microbenchmark(
   "1 core" = sg.int(example3, dim=3, lower=c(1,0,1), upper=c(20,10,20)),
-  "2 core" = sg.int.parallel(example3, dim=3, lower=c(1,0,1), upper=c(20,10,20), 
+  "2 core" = sg.int.parallel(example3, dim=3, lower=c(1,0,1), upper=c(20,10,20),
                              core=2),
   times=1
 )
@@ -191,7 +191,7 @@ example1 <- function(x){
   return(x[1]^3)
 }
 
-adaptIntegrate(example1,c(1),c(20)) # 39999.75
+adaptIntegrate(example1, c(1), c(20)) # 39999.75
 
 sg.int(example1, dim=1, lower=c(1), upper=c(20)) # 39999.75
 
@@ -202,7 +202,7 @@ microbenchmark(
 )
 # Unit: microseconds
 #            expr      min       lq      mean    median       uq       max neval cld
-#  adaptIntegrate  176.759  239.380  388.4196  279.3165  311.691  1716.067    20  a 
+#  adaptIntegrate  176.759  239.380  388.4196  279.3165  311.691  1716.067    20  a
 #          sg.int 5311.254 6631.268 8532.7894 7464.4895 9114.626 24610.033    20   b
 # the results are same, but adaptIntegrate is much faster
 
@@ -211,7 +211,7 @@ example2 <- function(x){
   return(x[1]^2 + x[2]^2)
 }
 
-adaptIntegrate(example2,c(1,0),c(20,10)) # 32996.67
+adaptIntegrate(example2, c(1,0), c(20,10)) # 32996.67
 
 sg.int(example2, dim=2, lower=c(1,0), upper=c(20,10)) # 32996.67
 
@@ -228,7 +228,7 @@ microbenchmark(
 
 # 3 dimensions
 example3 <- function(x){
-  return(x[1]^2 + x[2]^2 + x[3]) 
+  return(x[1]^2 + x[2]^2 + x[3])
 }
 
 adaptIntegrate(example3,c(1,0,1),c(20,10,20)) # 664841.7 (error = 1.164153e-10)
@@ -237,7 +237,7 @@ sg.int.parallel(example3, dim=3, lower=c(1,0,1), upper=c(20,10,20), core=2) # 78
 
 microbenchmark(
   "adaptIntegrate" = adaptIntegrate(example3,c(1,0,1),c(20,10,20)),
-  "sg.int" = sg.int.parallel(example3, dim=3, lower=c(1,0,1), upper=c(20,10,20), 
+  "sg.int" = sg.int.parallel(example3, dim=3, lower=c(1,0,1), upper=c(20,10,20),
                              core=2),
   times = 2
 )
@@ -252,3 +252,47 @@ microbenchmark(
 ##
 ## E. Monte Carlo integration
 ##
+MonteCarlo.int <- function(f, dim, lower, upper, core, n=10000){
+  require("parallel")
+  cluster <- makeCluster(core)
+
+  lower <- floor(lower)
+  upper <- ceiling(upper)
+  # if f is not function, return error
+  if(is.function(f)==FALSE){
+    stop("f should be a function!")
+  }
+  # if dim is not numeric, retrun error
+  if(is.numeric(dim)==FALSE){
+    stop("dim should be numeric!")
+  }
+  # if lower > upper, return error
+  if(any(lower > upper)){
+    stop("lower must be smaller than upper!")
+  }
+  u <- runif(n*dim, lower, upper)
+  these <- matrix(u, ncol=dim)
+  x <- parApply(cluster, these, 1, f)
+  return(mean(x)*(upper - lower)^dim)
+}
+
+# 1 dimension
+example1 <- function(x){
+  return(x[1]^3)
+}
+
+MonteCarlo.int(example1, dim=1, lower=c(1), upper=c(20), core=2)
+
+# 2 dimension
+example2 <- function(x){
+  return(x[1]^2 + x[2]^2)
+}
+
+MonteCarlo.int(example2, dim=2, lower=-3, upper=10, core=2)
+
+# 3 dimension
+example3 <- function(x){
+  return(x[1]^2 + x[2]^2 + x[3])
+}
+
+MonteCarlo.int(example3, dim=3, lower=-3, upper=10, core=2)
